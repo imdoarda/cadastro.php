31e2e23774f57c0f45092ea0f3e07639568a9c99
imdoarda <dudinha.mgalvao@gmail.com>
2024-06-11 09:19:06 -0300

    criação tela de inicio

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv='X-UA-Compatible' content="IE=edge">
+    <title>Maria Outfits</title>
+    <meta name='viewport" content=' width=device-width, initial-scale=1'>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <header id="cabecalho">
+        <nav id="menu">
+            <h1>Menu</h1>
+            <ul>
+                <li><a href="Index.php">Home</a></li>
+                <li><a href="cadastro.php">Cadastre-se</a></li>
+                <li><a href="login.php">Login</a></li>
+            </ul>
+        </nav>
+    </header>
+
+</body>
+
+</html>
