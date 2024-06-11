504e2a54efddb4114b7e3bb040c4311efd8d0afd
imdoarda <dudinha.mgalvao@gmail.com>
2024-06-11 11:05:21 -0300

    adiciona campo h1

--- index.php
+++ index.php
@@ -6,14 +6,12 @@
     <meta http-equiv='X-UA-Compatible' content="IE=edge">
     <title>Maria Outfits</title>
     <meta name='viewport" content=' width=device-width, initial-scale=1'>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" type="text/css" media="screen" href="style.css">
-    <link href="cover.css" rel="stylesheet">
+
 </head>
 
 <body>
+    <header>
         <nav id="menu">
             <h1>Menu</h1>
             <ul>
@@ -22,8 +20,9 @@
                 <li><a href="login.php">Login</a></li>
             </ul>
         </nav>
+    </header>
 
-        
+    <h1>BEM VINDO(A) AO NOSSO SITE</h1>
 
 </body>
 
