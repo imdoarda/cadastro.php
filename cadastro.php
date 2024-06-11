e382b17583bd83ec8021664792ac894a8a747e87
imdoarda <dudinha.mgalvao@gmail.com>
2024-06-11 11:05:03 -0300

    muda campo "form"

--- cadastro.php
+++ cadastro.php
@@ -6,16 +6,12 @@
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
     <title>Tela de Cadastro</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet">
     <script src='telas.js'></script>
     <link rel="stylesheet" type="text/css" media="screen" href="style.css">
-    <link href="cover.css" rel="stylesheet">
 </head>
 
 <body>
-
+    <header>
         <nav id="menu">
             <h1>Menu</h1>
             <ul>
@@ -24,30 +20,32 @@
                 <li><a href="login.php">Login</a></li>
             </ul>
         </nav>
-    <div class="container">
-        <h3> Tela de cadastro </h3>
-        <form>
-            <input type="email" id="loginEmail" placeholder="Digite seu email" />
-            <br>
-            <input type="password" id="loginSenha" placeholder="Digite sua senha" />
-            <br>
-            <input type="password" id="loginConfirm" placeholder="Confirme sua senha" />
-            <br>
-            <input type="name" id="loginName" placeholder="Digite seu nome" />
-            <br>
-            <input type="phone" id="loginTelefone" placeholder="Digite seu telefone" />
-            <br>
-            <input type="date" id="loginData" placeholder="Digite sua data de nascimento" />
-            <br>
-            <input type="ID" id="loginCPF" placeholder="Digite seu CPF" />
-            <br>
-            <br>
-            <button type="button" onclick="Verifica()">AVANÇAR</button>
-        </form>
+    </header>
+    <div id="login">
+        <form method="post" action="" class="form">
+            <h1> CADASTRO </h1>
+            <form>
+                <input type="email" id="loginEmail" placeholder="Digite seu email" />
+                <br>
+                <input type="password" id="loginSenha" placeholder="Digite sua senha" />
+                <br>
+                <input type="password" id="loginConfirm" placeholder="Confirme sua senha" />
+                <br>
+                <input type="name" id="loginName" placeholder="Digite seu nome" />
+                <br>
+                <input type="phone" id="loginTelefone" placeholder="Digite seu telefone" />
+                <br>
+                <input type="date" id="loginData" placeholder="Digite sua data de nascimento" />
+                <br>
+                <input type="ID" id="loginCPF" placeholder="Digite seu CPF" />
+                <br>
+                <br>
+                <button type="button" onclick="Verifica()">AVANÇAR</button>
+            </form>
 
     </div>
 
-    
+
 </body>
 
 </html>
